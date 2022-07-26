b81b9cfcbf4b4525aa4f647a2a56c7a5ce7201cc
jquihano <jeremy.quihano@gmail.com>
2022-07-26 14:33:11 -0700

    Adding variables and middleware

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+//-----
+//DECLARE VARIABLES
+//-----
 const express = require('express');
+const app = express();
+const PORT = 1234;
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+//-----
+//SET MIDDLEWARE
+//-----
+app.set("view engine", "ejs");
+app.use(express.static('publix'));
+app.use(express.urlencoded({extended: true}));
