fc691b3cb559e5b7298aa0b52e78f846721ee330
jquihano <jeremy.quihano@gmail.com>
2022-07-26 16:15:56 -0700

    Setting get route

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const express = require('express');
 const app = express();
 const PORT = 1234;
 const mongoose = require('mongoose');
+
 require('dotenv').config()
+const ToDoTask = require('./models/todotask')
+
 
 //-----
 //SET MIDDLEWARE
@@ -19,7 +22,15 @@ mongoose.connect(process.env.DB_CONNECTION,
     () => {console.log(`Connected to the db!`)}
 );
 
-
+app.get('/', async (req, res) => {
+    try {
+        ToDoTask.find({}, (err, tasks) => {
+            res.render('index.ejs', {todoTasks: tasks})
+        })
+    } catch (error) {
+        if(err) return res.status(500).send(err)
+    }
+})
 
 
 app.listen(PORT, () => {
