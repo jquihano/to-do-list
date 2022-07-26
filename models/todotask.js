c9c544c3221610dd1e94015517a5ac75f10d645c
jquihano <jeremy.quihano@gmail.com>
2022-07-26 15:48:31 -0700

    Exporting mongoose schema

--- models/todotask.js
+++ models/todotask.js
@@ -4,5 +4,15 @@ const todoTaskSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
-})
+});
+
+module.exports = mongoose.model('ToDoTasks', todoTaskSchema, 'tasks');
